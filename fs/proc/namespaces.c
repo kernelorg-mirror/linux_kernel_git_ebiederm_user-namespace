7d9e0ec043f198e3564d08d4c3961dc8f7963556
Eric W. Biederman <ebiederm@xmission.com>
2010-03-07 18:17:03 -0800

    pidns: Add setns support
    - Pid namespaces are designed to be inescapable so verify that the
      passed in pid namespace is a child of the currently active
      pid namespace or the currently active pid namespace itself.
    
      Allowing the currently active pid namespace is important so
      the effects of an earlier setns can be cancelled.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- fs/proc/namespaces.c
+++ fs/proc/namespaces.c
@@ -29,6 +29,9 @@ static const struct proc_ns_operations *ns_entries[] = {
 	&userns_operations,
 #endif
 	&mntns_operations,
+#ifdef CONFIG_PID_NS
+	&pidns_operations,
+#endif
 };
 
 static const struct file_operations ns_file_operations = {
