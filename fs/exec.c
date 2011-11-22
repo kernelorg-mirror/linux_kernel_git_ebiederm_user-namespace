6929096d32c122544877e82b046e48b8e496a32f
Eric W. Biederman <ebiederm@xmission.com>
2011-11-21 16:40:54 -0800

    userns: Ignore suid and sgid on binaries if the uid or gid can not be mapped
    When performing an exec where the binary lives in one user namespace and
    the execing process lives in another usre namespace there is the possibility
    that the target uids can not be represented.
    
    Instead of failing the exec simply ignore the suid/sgid bits and run
    the binary with lower privileges.   We already do this in the case
    of MNT_NOSUID so this should be a well tested code path.
    
    As the user and group are not changed this should not introduce any
    security issues.
    
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1288,14 +1288,13 @@ int prepare_binprm(struct linux_binprm *bprm)
 	bprm->cred->euid = current_euid();
 	bprm->cred->egid = current_egid();
 
-	if (!(bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID)) {
+	if (!(bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID) &&
+	    kuid_has_mapping(bprm->cred->user_ns, inode->i_uid) &&
+	    kgid_has_mapping(bprm->cred->user_ns, inode->i_gid)) {
 		/* Set-uid? */
 		if (mode & S_ISUID) {
-			if (!kuid_has_mapping(bprm->cred->user_ns, inode->i_uid))
-				return -EPERM;
 			bprm->per_clear |= PER_CLEAR_ON_SETID;
 			bprm->cred->euid = inode->i_uid;
-
 		}
 
 		/* Set-gid? */
@@ -1305,8 +1304,6 @@ int prepare_binprm(struct linux_binprm *bprm)
 		 * executable.
 		 */
 		if ((mode & (S_ISGID | S_IXGRP)) == (S_ISGID | S_IXGRP)) {
-			if (!kgid_has_mapping(bprm->cred->user_ns, inode->i_gid))
-				return -EPERM;
 			bprm->per_clear |= PER_CLEAR_ON_SETID;
 			bprm->cred->egid = inode->i_gid;
 		}
