e09aea7d7a404d442ba05e115e9fc0692d2441b1
Eric W. Biederman <ebiederm@xmission.com>
2012-07-26 00:29:53 -0700

    net: Enable unprivileged use of the network stack.
    Allow an unpriviled user who has created a user namespace, and then
    created a network namespace to effectively use the new network
    namespace, by reducing any capable(CAP_NET_ADMIN) and
    capable(CAP_NET_RAW) calls to be ns_capable(net->user_ns,
    CAP_NET_ADMIN), or capable(net->user_ns, CAP_NET_RAW) calls.
    
    This allows unprivileged users to user network stack services
    that are safe when isolated to a separate user namespace
    and network namespace instance.
    
    FIXME write a justficiation for why all of these calls are
    valid/safe for an unprivileged user to use.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- net/ipv4/ipip.c
+++ net/ipv4/ipip.c
@@ -663,7 +663,7 @@ ipip_tunnel_ioctl (struct net_device *dev, struct ifreq *ifr, int cmd)
 	case SIOCADDTUNNEL:
 	case SIOCCHGTUNNEL:
 		err = -EPERM;
-		if (!capable(CAP_NET_ADMIN))
+		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
 			goto done;
 
 		err = -EFAULT;
@@ -723,7 +723,7 @@ ipip_tunnel_ioctl (struct net_device *dev, struct ifreq *ifr, int cmd)
 
 	case SIOCDELTUNNEL:
 		err = -EPERM;
-		if (!capable(CAP_NET_ADMIN))
+		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
 			goto done;
 
 		if (dev == ipn->fb_tunnel_dev) {
