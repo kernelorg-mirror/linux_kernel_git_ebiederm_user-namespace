0fdef85de9766a6c0a6797137299ecacd293f9b7
Eric W. Biederman <ebiederm@xmission.com>
2012-07-27 06:21:27 -0700

    userns: Allow setting a userns mapping to your current uid.
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/user_namespace.c
+++ kernel/user_namespace.c
@@ -796,6 +796,21 @@ static __init int id_permissions_init(void)
 static bool new_idmap_permitted(struct user_namespace *ns, int cap_setid,
 				struct uid_gid_map *new_map)
 {
+	/* Allow mapping to your own filesystem ids */
+	if ((new_map->nr_extents == 1) && (new_map->extent[0].count == 1)) {
+		u32 id = new_map->extent[0].lower_first;
+		if (cap_setid == CAP_SETUID) {
+			kuid_t uid = make_kuid(ns->parent, id);
+			if (uid_eq(uid, current_fsuid()))
+				return true;
+		}
+		else if (cap_setid == CAP_SETGID) {
+			kgid_t gid = make_kgid(ns->parent, id);
+			if (gid_eq(gid, current_fsgid()))
+				return true;
+		}
+	}
+
 	if (new_idmap_permitted_by_key(ns, cap_setid, new_map))
 		return true;
 
