4eb5a07bee008586f95ec9d5a07ccbb07a0b11c2
Eric W. Biederman <ebiederm@xmission.com>
2012-08-01 06:54:43 -0700

    net: enable some rtnl and sysctls that are safe for unprivilged users
    - Only allow moving network devices to network namespaces you have
      CAP_NET_ADMIN privileges over.
    
    - Enable creating/deleting/modifying interfaces
    - Enable adding/deleting addresses
    - Enable adding/removing routes
    - Enable setting the forwarding state
    - Misc sysctls that look safe.
    
    Acked-by: Serge Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -2335,9 +2335,6 @@ static int inet6_rtm_delroute(struct sk_buff *skb, struct nlmsghdr* nlh, void *a
 	struct fib6_config cfg;
 	int err;
 
-	if (!capable(CAP_NET_ADMIN))
-		return -EPERM;
-
 	err = rtm_to_fib6_config(skb, nlh, &cfg);
 	if (err < 0)
 		return err;
@@ -2349,9 +2346,6 @@ static int inet6_rtm_newroute(struct sk_buff *skb, struct nlmsghdr* nlh, void *a
 {
 	struct fib6_config cfg;
 	int err;
-
-	if (!capable(CAP_NET_ADMIN))
-		return -EPERM;
 
 	err = rtm_to_fib6_config(skb, nlh, &cfg);
 	if (err < 0)
