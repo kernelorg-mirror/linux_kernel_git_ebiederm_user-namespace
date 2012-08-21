57fbe4a78c4c4e6070ee1ebe10facb88e60c6c32
Eric W. Biederman <ebiederm@xmission.com>
2012-08-20 23:28:58 -0700

    devpts: Set the default permissions of /dev/pts/ptmx and /dev/ptmx to 0666
    These are difference instances of the same device not so it only
    makes sense that they would have the same default permissions.
    
    Move the definitions of DEVPTS_DEEFAULT_PTMX_MODE and PTM_MINOR
    into devpts_fs.h so we can use the same definitions throughout.
    
    Acked-by: "Serge E. Hallyn" <serge@hallyn.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- drivers/tty/pty.c
+++ drivers/tty/pty.c
@@ -794,10 +794,10 @@ static void __init unix98_pty_init(void)
 	ptmx_fops.open = ptmx_open;
 
 	cdev_init(&ptmx_cdev, &ptmx_fops);
-	if (cdev_add(&ptmx_cdev, MKDEV(TTYAUX_MAJOR, 2), 1) ||
-	    register_chrdev_region(MKDEV(TTYAUX_MAJOR, 2), 1, "/dev/ptmx") < 0)
+	if (cdev_add(&ptmx_cdev, MKDEV(TTYAUX_MAJOR, PTMX_MINOR), 1) ||
+	    register_chrdev_region(MKDEV(TTYAUX_MAJOR, PTMX_MINOR), 1, "/dev/ptmx") < 0)
 		panic("Couldn't register /dev/ptmx driver");
-	device_create(tty_class, NULL, MKDEV(TTYAUX_MAJOR, 2), NULL, "ptmx");
+	device_create(tty_class, NULL, MKDEV(TTYAUX_MAJOR, PTMX_MINOR), NULL, "ptmx");
 }
 
 #else
