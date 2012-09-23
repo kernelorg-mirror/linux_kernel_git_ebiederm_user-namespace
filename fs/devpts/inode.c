abd52a55a5aaa5cac755e2bcdb6f73215d4a6bfb
Eric W. Biederman <ebiederm@xmission.com>
2012-09-23 05:09:23 -0700

    devpts: Remove the internal mount.
    Every mount of devpts creates a fresh super block so remove the
    internal mount.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -86,8 +86,6 @@ static struct ctl_table pty_root_table[] = {
 
 static DEFINE_MUTEX(allocated_ptys_lock);
 
-static struct vfsmount *devpts_mnt;
-
 struct pts_mount_opts {
 	int setuid;
 	int setgid;
@@ -385,12 +383,10 @@ static struct file_system_type devpts_fs_type = {
 
 struct vfsmount *devpts_mntget(struct file *filp)
 {
-	struct vfsmount *mnt;
+	struct vfsmount *mnt = ERR_PTR(-ENODEV);
 
 	if (filp->f_path.mnt->mnt_sb->s_magic == DEVPTS_SUPER_MAGIC)
 		mnt = mntget(filp->f_path.mnt);
-	else
-		mnt = mntget(devpts_mnt);
 	return mnt;
 }
 
@@ -551,15 +547,8 @@ static int __init init_devpts_fs(void)
 	int err = register_filesystem(&devpts_fs_type);
 	struct ctl_table_header *table;
 
-	if (!err) {
+	if (!err)
 		table = register_sysctl_table(pty_root_table);
-		devpts_mnt = kern_mount(&devpts_fs_type);
-		if (IS_ERR(devpts_mnt)) {
-			err = PTR_ERR(devpts_mnt);
-			unregister_filesystem(&devpts_fs_type);
-			unregister_sysctl_table(table);
-		}
-	}
 	return err;
 }
 module_init(init_devpts_fs)
