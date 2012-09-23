d1fc3c362eab0e10b39c589ce678b154186d4a04
Eric W. Biederman <ebiederm@xmission.com>
2012-09-23 03:05:23 -0700

    devpts: Remove the devpty cleanup special case.
    Move the call of devpts_kill_index from tty_release into
    ptm_unix98_cleanup.  We don't need a special magic hook anymore.
    
    Keep the mount point /dev/ptmx uses around in drvdata so we have
    a reference to which instance of devpts the pty master belongs
    to in ptm_unix98_cleanup.
    
    Add devpts_mntget to compute which instances of devpts we
    are using.
    
    Add ptm_unix98_cleanup (a standardy tty cleanup method) to perform
    final cleanup of unix98 ptys.  I use a cleanup method and not
    a remove or shutdown method because neither shutdown nor remove
    are allowed to sleep, and mntput can sleep.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- drivers/tty/pty.c
+++ drivers/tty/pty.c
@@ -4,6 +4,9 @@
  *  Added support for a Unix98-style ptmx device.
  *    -- C. Scott Ananian <cananian@alumni.princeton.edu>, 14-Jan-1998
  *
+ *  When reading this code see also fs/devpts. In particular note that the
+ *  driver_data field is used by the devpts side as a binding to the devpts
+ *  inode (pts) and the devpts mount (ptm).
  */
 
 #include <linux/module.h>
@@ -611,10 +614,15 @@ static void pty_unix98_remove(struct tty_driver *driver, struct tty_struct *tty)
 {
 }
 
-/* this is called once with whichever end is closed last */
-static void pty_unix98_shutdown(struct tty_struct *tty)
+static void ptm_unix98_cleanup(struct tty_struct *tty)
 {
-	devpts_kill_index(tty->driver_data, tty->index);
+	struct vfsmount *mnt = tty->driver_data;
+	if (mnt) {
+		/* Make this pty number available for reallocation */
+		devpts_kill_index(mnt, tty->index);
+		mntput(mnt);
+	}
+	pty_cleanup(tty);
 }
 
 static const struct tty_operations ptm_unix98_ops = {
@@ -631,8 +639,7 @@ static const struct tty_operations ptm_unix98_ops = {
 	.set_termios = pty_set_termios,
 	.ioctl = pty_unix98_ioctl,
 	.resize = pty_resize,
-	.shutdown = pty_unix98_shutdown,
-	.cleanup = pty_cleanup
+	.cleanup = ptm_unix98_cleanup,
 };
 
 static const struct tty_operations pty_unix98_ops = {
@@ -647,7 +654,6 @@ static const struct tty_operations pty_unix98_ops = {
 	.chars_in_buffer = pty_chars_in_buffer,
 	.unthrottle = pty_unthrottle,
 	.set_termios = pty_set_termios,
-	.shutdown = pty_unix98_shutdown,
 	.cleanup = pty_cleanup,
 };
 
@@ -667,6 +673,7 @@ static int ptmx_open(struct inode *inode, struct file *filp)
 {
 	struct tty_struct *tty;
 	struct inode *slave_inode;
+	struct vfsmount *mnt;
 	int retval;
 	int index;
 
@@ -675,9 +682,12 @@ static int ptmx_open(struct inode *inode, struct file *filp)
 	/* We refuse fsnotify events on ptmx, since it's a shared resource */
 	filp->f_mode |= FMODE_NONOTIFY;
 
+	/* Find the devpts instance we are working with */
+	mnt = devpts_mntget(filp);
+
 	retval = tty_alloc_file(filp);
 	if (retval)
-		return retval;
+		goto err_mnt;
 
 	/* find a device that is not in use. */
 	mutex_lock(&devpts_mutex);
@@ -706,6 +716,7 @@ static int ptmx_open(struct inode *inode, struct file *filp)
 	tty->driver_data = inode;
 
 	tty_add_file(tty, filp);
+	tty->driver_data = mnt;
 
 	slave_inode = devpts_pty_new(inode,
 			MKDEV(UNIX98_PTY_SLAVE_MAJOR, index), index,
@@ -728,9 +739,11 @@ err_release:
 	return retval;
 out:
 	mutex_unlock(&tty_mutex);
-	devpts_kill_index(inode, index);
+	devpts_kill_index(mnt, index);
 err_file:
 	tty_free_file(filp);
+err_mnt:
+	mntput(mnt);
 	return retval;
 }
 
