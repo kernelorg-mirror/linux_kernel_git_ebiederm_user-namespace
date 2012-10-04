72b49dc51b17c0f09435b8b203f70b21e0a708bc
Eric W. Biederman <ebiederm@xmission.com>
2012-10-04 13:42:34 -0700

    fuse: Teach fuse how to handle the pid namespace.
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -94,7 +94,7 @@ static void fuse_req_init_context(struct fuse_conn *fc, struct fuse_req *req)
 {
 	req->in.h.uid = from_kuid_munged(fc->user_ns, current_fsuid());
 	req->in.h.gid = from_kgid_munged(fc->user_ns, current_fsgid());
-	req->in.h.pid = current->pid;
+	req->in.h.pid = pid_nr_ns(task_pid(current), fc->pid_ns);
 }
 
 struct fuse_req *fuse_get_req(struct fuse_conn *fc)
