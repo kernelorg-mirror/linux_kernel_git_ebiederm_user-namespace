488414be4d49484a3ef7772e1a8cd90b7760c70d
Eric W. Biederman <ebiederm@xmission.com>
2012-10-10 17:25:00 -0700

    net: Allow opening an af_unix socket
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- net/nonet.c
+++ net/nonet.c
@@ -19,7 +19,7 @@ static int sock_no_open(struct inode *irrelevant, struct file *dontcare)
 	return -ENXIO;
 }
 
-const struct file_operations bad_sock_fops = {
+const struct file_operations socket_file_ops = {
 	.owner = THIS_MODULE,
 	.open = sock_no_open,
 	.llseek = noop_llseek,
