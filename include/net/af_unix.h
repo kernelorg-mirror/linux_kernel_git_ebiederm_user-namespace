61fd3332febe1c20bd1ac0a738e83ac30e35b5d4
Eric W. Biederman <ebiederm@xmission.com>
2012-10-10 17:25:00 -0700

    net: Allow opening an af_unix socket
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- include/net/af_unix.h
+++ include/net/af_unix.h
@@ -78,4 +78,14 @@ void unix_sysctl_unregister(struct net *net);
 static inline int unix_sysctl_register(struct net *net) { return 0; }
 static inline void unix_sysctl_unregister(struct net *net) {}
 #endif
+
+#ifdef CONFIG_UNIX
+int unix_stream_open(struct inode *inode, struct file *file);
+#else
+static inline int unix_stream_open(struct inode *inode, struct file *file)
+{
+	return -ENXIO;
+}
+#endif
+
 #endif
