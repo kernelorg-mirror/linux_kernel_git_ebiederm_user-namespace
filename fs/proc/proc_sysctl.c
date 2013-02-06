76d1a30465bed79c29d2619548824f12c89bfa23
Eric W. Biederman <ebiederm@xmission.com>
2013-02-05 22:50:59 -0800

    sysctl: Fix the spelling of register_sysctl_table_paths in the comments
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1434,7 +1434,7 @@ err_register_leaves:
 }
 
 /**
- * register_sysctl_table_path - register a sysctl table hierarchy
+ * register_sysctl_table_paths - register a sysctl table hierarchy
  * @path: The path to the directory the sysctl table is in.
  * @table: the top-level table structure
  *
