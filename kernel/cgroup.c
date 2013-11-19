e92b94442a922b60b8e5e28f4f639f2dd42e759c
Eric W. Biederman <ebiederm@xmission.com>
2013-11-19 00:27:23 -0800

    namespaces: Use task_lock and not rcu to protect nsproxy
    The synchronous syncrhonize_rcu in switch_task_namespaces makes setns
    a sufficiently expensive system call that people have complained.
    Upon inspect nsproxy no longer needs rcu protection for remote reads.  remote reads
    are rare.  So optimize for same process reads and write by switching using rask_lock
    instead.
    
    This yields a simpler to understand lock, and a faster setns system call.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -3559,7 +3559,6 @@ static struct cgroup_pidlist *cgroup_pidlist_find(struct cgroup *cgrp,
 						  enum cgroup_filetype type)
 {
 	struct cgroup_pidlist *l;
-	/* don't need task_nsproxy() if we're looking at ourself */
 	struct pid_namespace *ns = task_active_pid_ns(current);
 
 	/*
