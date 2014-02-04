f0d3dee8aac0dcf945a8076422796f38508e1578
Eric W. Biederman <ebiederm@xmission.com>
2014-02-03 20:00:29 -0800

    sysfs: Fix  sysfs_rename_link so it doesn't require a masters degree in sysfs to use.
    Symlinks should always be in the same namespace as their target
    object, making that optional is just confusing.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -212,9 +212,8 @@ int __must_check sysfs_create_link_nowarn(struct kobject *kobj,
 					  const char *name);
 void sysfs_remove_link(struct kobject *kobj, const char *name);
 
-int sysfs_rename_link_ns(struct kobject *kobj, struct kobject *target,
-			 const char *old_name, const char *new_name,
-			 const void *new_ns);
+int sysfs_rename_link(struct kobject *kobj, struct kobject *target,
+		      const char *old_name, const char *new_name);
 
 void sysfs_delete_link(struct kobject *dir, struct kobject *targ,
 			const char *name);
@@ -334,9 +333,8 @@ static inline void sysfs_remove_link(struct kobject *kobj, const char *name)
 {
 }
 
-static inline int sysfs_rename_link_ns(struct kobject *k, struct kobject *t,
-				       const char *old_name,
-				       const char *new_name, const void *ns)
+static inline int sysfs_rename_link(struct kobject *k, struct kobject *t,
+				    const char *old_name, const char *new_name)
 {
 	return 0;
 }
@@ -430,12 +428,6 @@ static inline void sysfs_remove_file(struct kobject *kobj,
 				     const struct attribute *attr)
 {
 	return sysfs_remove_file_ns(kobj, attr, NULL);
-}
-
-static inline int sysfs_rename_link(struct kobject *kobj, struct kobject *target,
-				    const char *old_name, const char *new_name)
-{
-	return sysfs_rename_link_ns(kobj, target, old_name, new_name, NULL);
 }
 
 static inline void sysfs_notify_dirent(struct kernfs_node *kn)
