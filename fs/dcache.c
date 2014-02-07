e951e87424deb2457a16b04e6a8d9d08313b80e7
Eric W. Biederman <ebiederm@xmission.com>
2014-02-06 16:56:40 -0800

    vfs: Simply when d_alloc_dname is called.
    Now that all implementations of d_dname use d_alloc_psuedo only
    consider calling d_dname on root dentries, as all dentries returned
    d_alloc_pseudo by d_alloc_pseudo are root dentries.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3037,18 +3037,14 @@ char *d_path(const struct path *path, char *buf, int buflen)
 	int error;
 
 	/*
-	 * We have various synthetic filesystems that never get mounted.  On
-	 * these filesystems dentries are never used for lookup purposes, and
-	 * thus don't need to be hashed.  They also don't need a name until a
-	 * user wants to identify the object in /proc/pid/fd/.  The little hack
-	 * below allows us to generate a name for these objects on demand:
-	 *
-	 * Some pseudo inodes are mountable.  When they are mounted
-	 * path->dentry == path->mnt->mnt_root.  In that case don't call d_dname
-	 * and instead have d_path return the mounted path.
+	 * We have various synthetic files allocated with
+	 * d_alloc_pseudo that are not available through
+	 * ordinary path lookup and don't need a name until
+	 * a user wants to identify the object in
+	 * /proc/pid/fd/ or similiar.
 	 */
 	if (path->dentry->d_op && path->dentry->d_op->d_dname &&
-	    (!IS_ROOT(path->dentry) || path->dentry != path->mnt->mnt_root))
+	    IS_ROOT(path->dentry) && path->dentry != path->mnt->mnt_root)
 		return path->dentry->d_op->d_dname(path->dentry, buf, buflen);
 
 	rcu_read_lock();
