2d5e4529d9e0d86314b0215eacc837dbbd729c0d
Eric W. Biederman <ebiederm@xmission.com>
2017-04-11 10:56:40 -0500

    pid: Teach find_task_by_pid_ns to look first at pid then at tgid
    For the short term this changes nothing, as when something can be
    found with PIDTYPE_TGID it can also be found with PIDTYPE_PID.
    
    In the future when we actually allow thread group leaders to exit
    and be reaped having the PIDTYPE_TGID probe will be necessary
    to keep the code functioning as it does today.
    
    Probing first by PIDTYPE_PID guarantees that whichever thread winds up
    being the accessible with pid_task(pid, PIDTYPE_TGID) the code will
    always find a thread with the specified pid if it exists.  This leaves
    the PIDTYPE_TGID test as simply a fallback to keep things working when
    tgids are specified.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/pid.c
+++ kernel/pid.c
@@ -454,9 +454,18 @@ EXPORT_SYMBOL(pid_task);
  */
 struct task_struct *find_task_by_pid_ns(pid_t nr, struct pid_namespace *ns)
 {
+	struct task_struct *task;
+	struct pid *pid;
+
 	RCU_LOCKDEP_WARN(!rcu_read_lock_held(),
 			 "find_task_by_pid_ns() needs rcu_read_lock() protection");
-	return pid_task(find_pid_ns(nr, ns), PIDTYPE_PID);
+
+	pid = find_pid_ns(nr, ns);
+	task = pid_task(pid, PIDTYPE_PID);
+	if (!task)
+		task = pid_task(pid, PIDTYPE_TGID);
+
+	return task;
 }
 
 struct task_struct *find_task_by_vpid(pid_t vnr)
