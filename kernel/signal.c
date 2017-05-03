09d738fa029ec64755396f00f19a3fb71172326b
Eric W. Biederman <ebiederm@xmission.com>
2017-05-02 21:56:16 -0500

    signal: In zap_other_threads us for_each_thread
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1208,14 +1208,17 @@ force_sig_info(int sig, struct siginfo *info, struct task_struct *t)
  */
 int zap_other_threads(struct task_struct *p)
 {
-	struct task_struct *t = p;
+	struct task_struct *t;
 	bool consume;
 	int count = 0;
 
 	consume = (p->signal->group_stop_count) &&
 		(p->jobctl & JOBCTL_STOP_CONSUME);
 
-	while_each_thread(p, t) {
+	for_each_thread(p, t) {
+		if (t == p)
+			continue;
+
 		if (consume && (t->jobctl & JOBCTL_STOP_PENDING))
 			WARN_ON_ONCE(task_participate_group_stop(t));
 		task_clear_jobctl_pending(t, JOBCTL_PENDING_MASK);
