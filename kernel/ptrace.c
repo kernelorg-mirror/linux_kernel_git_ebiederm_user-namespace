6d4983b18cf0eeaef47b705351c4350d6da43692
Eric W. Biederman <ebiederm@xmission.com>
2017-05-03 14:31:56 -0500

    ptrace: Make it clear the code is talking about threads
    Don't use find_task_by_vpid which can lookup a task
    either by it's thread id or it's process id.  Instead use:
    
        pid_task(find_vpid(pid), PIDTYPE_PID)
    
    to explicitly lookup a task by it's thread id.
    
    This will make a runtime difference when the kernel
    starts support reaping of zombie group leaders.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -1080,7 +1080,7 @@ static struct task_struct *ptrace_get_task_struct(pid_t pid)
 	struct task_struct *child;
 
 	rcu_read_lock();
-	child = find_task_by_vpid(pid);
+	child = pid_task(find_vpid(pid), PIDTYPE_PID);
 	if (child)
 		get_task_struct(child);
 	rcu_read_unlock();
