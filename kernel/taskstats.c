d40cab41244e8806a9f9d99ca3b1bd5cd7d5a375
Eric W. Biederman <ebiederm@xmission.com>
2017-05-26 06:30:57 -0500

    taskstats: Replace while_each_thread with for_each_thread
    while_each_thread is deprecated and going away.  for_each_thread
    in this case is slightly simpler and a drop in replacement
    so use it instead.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/taskstats.c
+++ kernel/taskstats.c
@@ -230,9 +230,8 @@ static int fill_stats_for_tgid(pid_t tgid, struct taskstats *stats)
 	else
 		memset(stats, 0, sizeof(*stats));
 
-	tsk = first;
 	start_time = ktime_get_ns();
-	do {
+	for_each_thread(first, tsk) {
 		if (tsk->exit_state)
 			continue;
 		/*
@@ -255,7 +254,7 @@ static int fill_stats_for_tgid(pid_t tgid, struct taskstats *stats)
 
 		stats->nvcsw += tsk->nvcsw;
 		stats->nivcsw += tsk->nivcsw;
-	} while_each_thread(first, tsk);
+	}
 
 	spin_unlock_irqrestore(&first->signal->siglock, flags);
 	rc = 0;
