d1251b02d4e61a706a876088bc4c919c373b086a
Eric W. Biederman <ebiederm@xmission.com>
2017-05-30 11:48:03 -0500

    exit: Don't let find_alive_thread return it's starting thread
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -483,7 +483,7 @@ static struct task_struct *find_alive_thread(struct task_struct *p)
 	struct task_struct *t;
 
 	for_each_thread(p, t) {
-		if (!(t->flags & PF_EXITING))
+		if ((t != p) && !(t->flags & PF_EXITING))
 			return t;
 	}
 	return NULL;
