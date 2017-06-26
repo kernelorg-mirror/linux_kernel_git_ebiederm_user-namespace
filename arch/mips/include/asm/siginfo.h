2a4717835844e061ae672a5f266ca5d77d10b4d2
Eric W. Biederman <ebiederm@xmission.com>
2017-06-26 16:16:17 -0500

    signal/mips: Document a conflict with SI_USER with SIGFPE
    Setting si_code to __SI_FAULT results in a userspace seeing
    an si_code of 0.  This is the same si_code as SI_USER.  Posix
    and common sense requires that SI_USER not be a signal specific
    si_code.  As such this use of 0 for the si_code is a pretty
    horribly broken ABI.
    
    This use of of __SI_FAULT is only a decade old.  Which compared
    to the other pieces of kernel code that has made this mistake
    is almost yesterday.
    
    This is probably worth fixing but I don't know mips well enough
    to know what si_code to would be the proper one to use.
    
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Ref: 948a34cf3988 ("[MIPS] Maintain si_code field properly for FP exceptions")
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

---
+++ arch/mips/include/asm/siginfo.h
@@ -0,0 +1,11 @@
+#ifndef _ASM_SIGINFO_H
+#define _ASM_SIGINFO_H
+
+#include <uapi/asm/siginfo.h>
+
+/*
+ * SIGFPE si_codes
+ */
+#define FPE_FIXME	(__SI_FAULT|0)	/* Broken dup of SI_USER */
+
+#endif /* _ASM_SIGINFO_H */
