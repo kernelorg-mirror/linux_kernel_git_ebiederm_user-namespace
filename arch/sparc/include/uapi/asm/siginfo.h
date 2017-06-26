a4e856ca07d5cec215b99e8ef5a03ce85347139b
Eric W. Biederman <ebiederm@xmission.com>
2017-06-26 16:07:22 -0500

    signal/sparc: Document a conflict with SI_USER with SIGFPE
    Setting si_code to __SI_FAULT results in a userspace seeing
    an si_code of 0.  This is the same si_code as SI_USER.  Posix
    and common sense requires that SI_USER not be a signal specific
    si_code.  As such this use of 0 for the si_code is a pretty
    horribly broken ABI.
    
    This was introduced in 2.3.41 so this mess has had a long time for
    people to be able to start depending on it.
    
    As this bug has existed for 17 years already I don't know if it is
    worth fixing.  It is definitely worth documenting what is going
    on so that no one decides to copy this bad decision.
    
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: sparclinux@vger.kernel.org
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- arch/sparc/include/uapi/asm/siginfo.h
+++ arch/sparc/include/uapi/asm/siginfo.h
@@ -17,6 +17,11 @@
 #define SI_NOINFO	32767		/* no information in siginfo_t */
 
 /*
+ * SIGFPE si_codes
+ */
+#define FPE_FIXME	(__SI_FAULT|0)	/* Broken dup of SI_USER */
+
+/*
  * SIGEMT si_codes
  */
 #define EMT_TAGOVF	(__SI_FAULT|1)	/* tag overflow */
