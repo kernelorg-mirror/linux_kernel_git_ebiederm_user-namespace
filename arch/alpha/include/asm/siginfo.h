83ca37cf99196c25b0ba52b86d60e87e438e6e02
Eric W. Biederman <ebiederm@xmission.com>
2017-06-26 13:38:04 -0500

    signal/alpha: Document a conflict with SI_USER for SIGTRAP
    Setting si_code to __SI_FAULT results in a userspace seeing
    an si_code of 0.  This is the same si_code as SI_USER.  Posix
    and common sense requires that SI_USER not be a signal specific
    si_code.  As such this use of 0 for the si_code is a pretty
    horribly broken ABI.
    
    Given that alpha is on it's last legs I don't know that it is worth
    fixing this, but it is worth documenting what is going on so that
    no one decides to copy this bad decision.
    
    This was introduced during the 2.5 development cycle so this
    mess has had a long time for people to be able to depend upon it.
    
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: linux-alpha@vger.kernel.org
    History Tree: https://git.kernel.org/pub/scm/linux/kernel/git/tglx/history.git
    Ref: 0a635c7a84cf ("Fill in siginfo_t.")
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

---
+++ arch/alpha/include/asm/siginfo.h
@@ -0,0 +1,11 @@
+#ifndef __ASM_ALPHA_SIGINFO_H
+#define __ASM_ALPHA_SIGINFO_H
+
+#include <uapi/asm/siginfo.h>
+
+/*
+ * SIGTRAP si_codes
+ */
+#define TRAP_FIXME	(__SI_FAULT|0)	/* Broken dup of SI_USER */
+
+#endif /* __ASM_ALPHA_SIGINFO_H */
