8bf0b2e6f15f6e299354c44543ebfe831935259f
Eric W. Biederman <ebiederm@xmission.com>
2017-08-02 14:16:08 -0500

    signal/arm/kvm: Use send_sig_mceerr
    This simplifies the code making it clearer what is going on,
    and making the siginfo generation easier to maintain.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- virt/kvm/arm/mmu.c
+++ virt/kvm/arm/mmu.c
@@ -1266,19 +1266,14 @@ static void coherent_cache_guest_page(struct kvm_vcpu *vcpu, kvm_pfn_t pfn,
 static void kvm_send_hwpoison_signal(unsigned long address,
 				     struct vm_area_struct *vma)
 {
-	siginfo_t info;
-
-	info.si_signo   = SIGBUS;
-	info.si_errno   = 0;
-	info.si_code    = BUS_MCEERR_AR;
-	info.si_addr    = (void __user *)address;
+	short lsb;
 
 	if (is_vm_hugetlb_page(vma))
-		info.si_addr_lsb = huge_page_shift(hstate_vma(vma));
+		lsb = huge_page_shift(hstate_vma(vma));
 	else
-		info.si_addr_lsb = PAGE_SHIFT;
+		lsb = PAGE_SHIFT;
 
-	send_sig_info(SIGBUS, &info, current);
+	send_sig_mceerr(BUS_MCEERR_AR, (void __user *)address, lsb, current);
 }
 
 static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
