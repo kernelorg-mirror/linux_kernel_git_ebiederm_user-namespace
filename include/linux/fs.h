f580ac03c7d8ca0e2ba7a5950b6a442a1afab94c
David Howells <dhowells@redhat.com>
2018-10-01 16:06:46 +0200

    vfs: Add support for the new parameter parser to register_filesystem
    This is a small piece of "vfs: Implement a filesystem superblock creation/configuration context [ver #12]"
    by David Howells <dhowells@redhat.com> reworked to stand by itself. --EWB
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2118,6 +2118,7 @@ struct file_system_type {
 #define FS_HAS_SUBTYPE		4
 #define FS_USERNS_MOUNT		8	/* Can be mounted by userns root */
 #define FS_RENAME_DOES_D_MOVE	32768	/* FS will handle d_move() during rename() internally. */
+	const struct fs_parameter_description *parameters;
 	struct dentry *(*mount) (struct file_system_type *, int,
 		       const char *, void *);
 	void (*kill_sb) (struct super_block *);
