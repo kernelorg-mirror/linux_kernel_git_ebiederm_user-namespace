c1c67620b030b85a50d8b81c554d43d199bb93c3
Eric W. Biederman <ebiederm@xmission.com>
2019-01-04 08:30:06 -0600

    fs/cifs: Use sget_userns to remove the need for SB_SUBMOUNT
    The only function the SB_SUBMOUNT flag retains is to convince sget to
    call sget_userns with &init_user_ns.  Perform that work explicitly in
    cifs_smb3_do_mount instead and remove the need for cifs to use the
    SB_SUBMOUNT flag.
    
    As today all cifs mounts are in the init_user_ns which means that work
    amounts to always passing &init_user_ns.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/cifs/cifs_dfs_ref.c
+++ fs/cifs/cifs_dfs_ref.c
@@ -237,7 +237,7 @@ static struct dentry *cifs_dfs_do_refmount(struct dentry *mntpt,
 	if (IS_ERR(mountdata))
 		return ERR_CAST(mountdata);
 
-	root = cifs_do_mount(&cifs_fs_type, SB_SUBMOUNT, devname, mountdata);
+	root = cifs_do_mount(&cifs_fs_type, 0, devname, mountdata);
 	root = finish_subsuper(mntpt->d_sb, root);
 	kfree(mountdata);
 	kfree(devname);
